f8998258d5a913499b80cf1ab108343dbfa56a6b
Impulse2020 <irishht@gmail.com>
2020-06-03 05:16:47 -0400

    Mvp

--- github-usercard/src/Card.js
+++ github-usercard/src/Card.js
@@ -1,26 +1,26 @@
 import React from 'react';
 import axios from 'axios';
 
-const Card = (props) =>{
+const Card = (props) => {
 
-return(
-<div className="card">
+    return (
+        <div className="card">
 
-{console.log(props)}
-<h1>{props.name}</h1>
-<img src={props.picture} alt="my face" />
-<h3>{props.user}</h3>
-<button onClick={getFollowers}>Show followers</button>
-<p>{props.bio}</p>
-</div>
-)
+            {console.log(props)}
+            <h1>{props.name}</h1>
+            <img src={props.picture} alt="my face" />
+            <h3>{props.user}</h3>
+            <button onClick={getFollowers}>Show followers</button>
+            <p>{props.bio}</p>
+        </div>
+    )
 
 }
 const getFollowers = e => {
     axios.get('https://api.github.com/users/impulse2020/followers')
         .then(response => {
-            for(let i = 0; i<response.data.length;i++){
-                console.log(response.data[i])
+            for (let i = 0; i < response.data.length; i++) {
+
             }
 
         })
